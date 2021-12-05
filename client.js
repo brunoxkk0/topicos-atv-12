5441bebcee54085b55ebdeb9af08133268d58932
Bruno <cocacola_bruno@hotmail.com>
2021-12-05 18:18:16 -0300

    Exercício 2

--- client.js
+++ client.js
@@ -1,52 +1,53 @@
 const axios = require('axios').default;
 
-axios.post('http://localhost:3000/books', {
-    ID: 3,
-    name: 'My Boook',
-    author: 'Me and only me'
-}).then((res) => {
-    console.log(res.data);
-}).catch(err => {
-    console.log(err.response.data);
-});
+async function addBook(){
+    await axios.post('http://localhost:3000/books', {
+        ID: 3,
+        name: 'My Boook',
+        author: 'Me and only me'
+    }).then((res) => {
+        console.log(res.data);
+    }).catch(err => {
+        console.log(err.response.data);
+    });
+}
 
-// Atividade 1
-axios.post('http://localhost:3000/books', {
-    ID: 4,
-    name: 'Novo Livro',
-    author: 'Desconhecido'
-}).then((res) => {
-    console.log(res.data);
-}).catch(err => {
-    console.log(err.response.data);
-});
-
-axios.post('http://localhost:3000/books', {
-    ID: 4,
-    name: 'Novo Livro 2',
-    author: 'Desconhecido'
-}).then((res) => {
-    console.log(res.data);
-}).catch(err => {
-    console.log(err.response.data);
-});
+async function addNewBook(){
+    // Atividade 1
+    await axios.post('http://localhost:3000/books', {
+        ID: 4,
+        name: 'Novo Livro',
+        author: 'Desconhecido'
+    }).then((res) => {
+        console.log(res.data);
+    }).catch(err => {
+        console.log(err.response.data);
+    });
+}
 
 
 // Atividade 2
-axios.get('http://localhost:3000/books')
-    .then((response) => {
-
-        console.log(response.data);
-
-        response.data.forEach(livro => {
-            axios.get('http://localhost:3000/books/' + livro.ID).then((book) => {
-                console.log(book.data)
+async function showBooks(){
+    await axios.get('http://localhost:3000/books')
+        .then((response) => {
+            console.log(response.data);
+            response.data.forEach(livro => {
+                axios.get('http://localhost:3000/books/' + livro.ID).then((book) => {
+                    console.log(book.data)
+                })
             })
-        })
-
-    });
+        });
+}
 
 // Atividade 4
-axios.get('http://localhost:3000/log').then((response) => {
-    console.log(response)
-})
+async function checkLog(){
+    await axios.get('http://localhost:3000/log').then((response) => {
+        console.log(response.data)
+    })
+}
+
+addBook();
+addNewBook();
+addNewBook();
+showBooks();
+checkLog();
